caeb02cafe643da2ef64809c50fcf31e49fea439
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2023-11-06 22:51:36 +0100

    Sorting out numbers , symbols and letters

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post">
+        <label for="sentence">Enter a sentence: </label>
+        <input type="text" name="sentence" id="sentence">
+        <input type="submit" value="Finish">
+    </form>
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $sentence = $_POST["sentence"];
+
+        $letters = $numbers = $symbols = '';
+
+        for ($i = 0; $i < strlen($sentence); $i++) {
+            $char = $sentence[$i];
+            if (is_numeric($char)) {
+                $numbers .= $char;
+            } elseif (ctype_alpha($char)) {
+                $letters .= $char;
+            } else {
+                $symbols .= $char;
+            }
+        }
+    }
+    ?>
+    
+</body>
+</html>
